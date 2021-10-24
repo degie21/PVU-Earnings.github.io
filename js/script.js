ec7542e925e828c93cc5527bc939c1562eae6d99
degie21 <degz141@gmail.com>
2021-10-24 20:26:16 +0800

    v 0.2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var sunMama = [], sunSap = [], smallPot = [], largePot = [], water = [], scareCrow = [] , monthlyProduction = 0, nftPlant = [],nftPlantCounter, nonNFTPlantCounter = 5, nftMTCounter, nonNFTMTCounter = 1;
-sunMama = {"class" : "sMama" , "type": "non-nft Plant", "name" : "Sunflower Mama" ,"cost" : 200, "generatedLE" : 850, "daysToProduce" : 6};
-sunSap = {"class" : "sSap" , "type": "non-nft Plant" , "name" : "Sunflower Sappling", "cost" : 200, "generatedLE" : 250, "daysToProduce" : 3};
+var sunMama = [], sunSap = [], smallPot = [], largePot = [], water = [], scareCrow = [] , monthlyProduction = 0, nftPlant = [],nftPlantCounter, nonNFTPlantCounter = 0, nonNFTCounter = 0, nonNFTMotherTreeCounter = 0;
+sunMama = {"plant_id" : 0 , "plantClass" : "'sMama'" , "type": "'nonNFT'", "name" : "'Sunflower Mama'" ,"cost" : 200, "generatedLE" : 850, "daysToProduce" : 6};
+sunSap = {"plant_id" : 0 , "plantClass" : "'sSapling'" , "type": "'nonNFT'" , "name" : "'Sunflower Sappling'", "cost" : 200, "generatedLE" : 250, "daysToProduce" : 3};
 smallPot = {"class" : "smPot" , "type": "tool" , "name" : "Small Pot", cost : 50, "quantity" : 1};
 largePot = {"class" : "lgPot" , "type": "tool" , "name" : "Large Pot", cost : 100, "quantity" : 1};
 water = {"class" : "water" , "type": "tool" , "name" : "Water", cost : 50, "quantity" : 100};
@@ -22,13 +22,42 @@ function pvuRefresh(){
 }
 
 function plantPreview(plant = [], divId){
+    console.log(plant);
     document.getElementById(divId).innerHTML =
-    '<div class"'+ plant.class + '">'+
+    '<div id"'+ plant.plantClass + '">'+
         '<div class = "plantName">Name : ' + plant.name + '</div>' +
         '<div class = "plantCost">Cost : ' + plant.cost + '</div>' +
-        '<div class = "plantProduction">Generated LE : ' + plant.generatedLE + 'LE' + ' / ' + plant.daysToProduce + ' days'
+        '<div class = "plantProduction">Generated LE : ' + plant.generatedLE + 'LE' + ' / ' + plant.daysToProduce + ' days' +
+        '<div class = "addPlant"><input type="submit" value="Add Plant" onclick="addPlant('+ plant.plantClass +',' + plant.name +',' + plant.type + ',' + plant.generatedLE + ',' + plant.daysToProduce +')"></div>' +
     '</div>';
 }
+
+function addPlant(plantclass, name, type, generatedLE, daysToProduce){
+    console.log(plantclass);
+    nonNFTCounter += 1;
+    if(plantclass == "sMama"){
+        nonNFTMotherTreeCounter += 1;
+    }else{
+        nonNFTPlantCounter += 1;
+    }
+    console.log(nonNFTPlantCounter + "," + nonNFTMotherTreeCounter)
+    document.getElementById("shownonNFT").innerHTML +=
+    '<div class="nftPlant" id="nonNFT' + nonNFTCounter + '">'+
+        '<div class = "plantID">ID : ' + name + '</div>' +
+        '<div class = "plantNFTtype">NFT Type : ' + type + '</div>' +
+        '<div class = "plantProduction">Generated LE : ' + generatedLE + 'LE' + ' / ' + daysToProduce + ' days |</div>' +
+        '<input type="submit" value="x" onclick="removenonNFTPlant(' + nonNFTCounter + ' )">'+
+    '</div>';
+    pvuRefresh();
+    return;
+}
+
+function removenonNFTPlant(nonNFTCounter){
+    var plantDiv = "nonNFT" + nonNFTCounter;
+    console.log(plantDiv);
+    document.getElementById(plantDiv).outerHTML ="";
+}
+
 function nftPlantPreview(plant = [], divId){
     var  cur_nftType = "'"+plant.nft_type+"'";
     document.getElementById(divId).innerHTML +=
@@ -43,11 +72,6 @@ function nftPlantPreview(plant = [], divId){
 
 function removePlant(nftPlantID,nft_type){
     console.log(nftPlantID , nft_type);
-    if(nft_type == "mother tree"){
-        nonNFTMTCounter += 1;
-    }else{
-        nonNFTPlantCounter += 1;
-    }
     nftPlant.splice(nftPlantID, 1);
     pvuRefresh();
 }
@@ -59,44 +83,22 @@ function leMonthlyProduction(plant = []){
 }
 
 function addNFT(plantID, nftType, plantType, generatedLE, daysToProduce){
-    if(plantID == "" || nftType == "" || plantType == "" || generatedLE == "" || daysToProduce == "" ){
-        return;
-    }
-    if(nftType == "Mother Tree"){
-        nonNFTMTCounter -= 1;
-        if(nonNFTMTCounter < 0){
-            alert("sobra Mother tree");
-            return;
-        }
-    }else{
-        nonNFTPlantCounter -= 1;
-        console.log(nonNFTPlantCounter);
-        if(nonNFTPlantCounter < 0){
-            alert("sobra Plant");
-            return;
-        }
-    }
+    // if(plantID == "" || nftType == "" || plantType == "" || generatedLE == "" || daysToProduce == "" ){
+    //     return;
+    // }
     nftPlantID = nftPlant.length;
-    nftPlant = nftPlant.concat({"id" : nftPlantID, "plant_id" : plantID, "nft_type" : nftType, "plant_type" : plantType, "generatedLE" : generatedLE, "daysToProduce": daysToProduce});
+    nftPlant = nftPlant.concat({"id" : nftPlantID, "plant_id" : plantID, "nft_type" : nftType, "type" : plantType, "generatedLE" : generatedLE, "daysToProduce": daysToProduce});
     pvuRefresh();
 }
 
 function monthlyEarning(){
-    if(nonNFTMTCounter > 0){
-        leMonthlyProduction(sunMama);
-    }else{
-        for(let i = 0; i < nftPlant.length; i++){
-            if(nftPlant[i].nft_type == "Mother Tree"){
-                leMonthlyProduction(nftPlant[i]);
-            }
-        }
-    }
-        for(let i = nonNFTPlantCounter; i > 0; i--){
-            leMonthlyProduction(sunSap);
-        }
     for(let i = 0; i < nftPlant.length; i++){
-        if(nftPlant[i].nft_type == "Plant"){
             leMonthlyProduction(nftPlant[i]);
-        }
+    }
+    for(let i = 0; i < nonNFTPlantCounter; i++){
+        leMonthlyProduction(sunSap);
+    }
+    for(let i = 0; i < nonNFTMotherTreeCounter; i++){
+        leMonthlyProduction(sunMama);
     }
 }
